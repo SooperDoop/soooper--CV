93d37b879640dd921e5d889d866285b323c26ed1
Shiva santosh <34417532+SooperDoop@users.noreply.github.com>
2019-04-10 22:01:42 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# camera.py
+
+import cv2
+
+class VideoCamera(object):
+    
+
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+#            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+
+    # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            
+        ret, jpeg = cv2.imencode('.jpg', image)
+        
+        
+        return jpeg.tobytes()
